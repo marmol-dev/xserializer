bed4bda40d27c45abe7c828f88d59fc7a433cb87
tomymolina <tm07081995@gmail.com>
2016-11-27 14:25:59 +0100

    Add declarations in tsconfig

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./deserializer'));
-__export(require('./serializer'));
-__export(require('./deserializable'));
-__export(require('./serializable'));
